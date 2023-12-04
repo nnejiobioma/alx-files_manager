91f2de66e28ffdc6fb6a1f88083a4b8b67a7ac04
obiomanneji <nnejiobioma@gmail.com>
2023-12-04 15:39:52 +0100

    task created and files added to project

---
+++ utils/redis.js
@@ -0,0 +1,55 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    // Handle errors and log them to the console
+    this.client.on('error', (err) => {
+      console.error(`Redis Client Error: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+}
+
+// Create and export an instance of RedisClient called redisClient
+const redisClient = new RedisClient();
+module.exports = redisClient;
